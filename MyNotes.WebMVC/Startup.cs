25b23e4b4de46b73bd8a63ade67b52d138929324
Dinara Abakirova <abakirovada@gmail.com>
2021-09-29 12:22:30 -0400

    Add project files.

---
+++ MyNotes.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyNotes.WebMVC.Startup))]
+namespace MyNotes.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
